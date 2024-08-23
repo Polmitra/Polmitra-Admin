fcf4f1add6de4ee907e232d810d32c67cb7646cb
Sushil Kumar <72073576+voltzylex@users.noreply.github.com>
2024-08-23 14:59:34 +0530

    fix event model

--- lib/models/event.dart
+++ lib/models/event.dart
@@ -3,6 +3,7 @@ import 'package:polmitra_admin/models/indian_city.dart';
 import 'package:polmitra_admin/models/indian_state.dart';
 import 'package:polmitra_admin/models/user.dart';
 
+
 class Event {
   final String id;
   final String eventName;
@@ -20,10 +21,14 @@ class Event {
   final Timestamp? updatedAt;
   final IndianState state;
   final IndianCity city;
+  final int? endingTime;
+  final String? endingImageUrl;
+  final Map<String, double>? endingImageLocation; // New field for location
 
   String get netaDisplayName => neta?.name ?? neta?.email ?? netaId;
 
-  String get karyakartaDisplayName => karyakarta?.name ?? karyakarta?.email ?? karyakartaId;
+  String get karyakartaDisplayName =>
+      karyakarta?.name ?? karyakarta?.email ?? karyakartaId;
 
   Event(
       {required this.id,
@@ -41,30 +46,41 @@ class Event {
       this.createdAt,
       this.updatedAt,
       required this.state,
-      required this.city});
+      required this.city,
+      this.endingImageUrl,
+      this.endingTime,
+      this.endingImageLocation});
 
   factory Event.fromDocument(DocumentSnapshot doc) {
     return Event(
-        id: doc.id,
-        eventName: doc['eventName'] ?? '',
-        description: doc['description'] ?? '',
-        date: doc['date'] ?? '',
-        time: doc['time'] ?? '',
-        address: doc['address'] ?? '',
-        images: List<String>.from(doc['images'] ?? []),
-        karyakartaId: doc['karyakartaId'] ?? '',
-        netaId: doc['netaId'] ?? '',
-        isActive: doc['isActive'] ?? true,
-        createdAt: doc['createdAt'],
-        updatedAt: doc['updatedAt'],
-        karyakarta: doc['karyakarta'] != null ? PolmitraUser.fromMap(doc['karyakarta']) : null,
-        neta: doc['neta'] != null
-            ? PolmitraUser.fromMap(
-                doc['neta'],
-              )
-            : null,
-        state: IndianState.fromMap(doc['state']),
-        city: IndianCity.fromMap(doc['city']));
+      id: doc.id,
+      eventName: doc['eventName'] ?? '',
+      description: doc['description'] ?? '',
+      date: doc['date'] ?? '',
+      time: doc['time'] ?? '',
+      address: doc['address'] ?? '',
+      images: List<String>.from(doc['images'] ?? []),
+      karyakartaId: doc['karyakartaId'] ?? '',
+      netaId: doc['netaId'] ?? '',
+      isActive: doc['isActive'] ?? true,
+      createdAt: doc['createdAt'],
+      updatedAt: doc['updatedAt'],
+      endingTime: doc["ending_time"] ?? 0,
+      karyakarta: doc['karyakarta'] != null
+          ? PolmitraUser.fromMap(doc['karyakarta'])
+          : null,
+      neta: doc['neta'] != null ? PolmitraUser.fromMap(doc['neta']) : null,
+      state: doc["state"] != null
+          ? IndianState.fromMap(doc['state'])
+          : IndianState(stateid: 1, statename: "None", cities: []),
+      city: IndianCity.fromMap(doc['city']),
+
+      endingImageUrl:
+          doc["endingImageUrl"], // Default to an empty string if not present
+      endingImageLocation: doc['endingImageLocation'] != null
+          ? Map<String, double>.from(doc['endingImageLocation'])
+          : null, // Default to null if not present
+    );
   }
 
   Map<String, dynamic> toJson() {
@@ -84,6 +100,10 @@ class Event {
       'karyakarta': karyakarta?.toMap(),
       'state': state.toMap(),
       'city': city.toMap(),
+      "ending_time": endingTime,
+
+      "endingImageUrl": endingImageUrl,
+      "endingImageLocation": endingImageLocation, // Added field
     };
   }
 
@@ -102,7 +122,10 @@ class Event {
       PolmitraUser? neta,
       Timestamp? updatedAt,
       IndianState? state,
-      IndianCity? city}) {
+      IndianCity? city,
+      String? endingImageUrl,
+      int? endingTime,
+      Map<String, double>? endingImageLocation}) {
     return Event(
       id: id ?? this.id,
       eventName: eventName ?? this.eventName,
@@ -120,6 +143,10 @@ class Event {
       updatedAt: updatedAt ?? this.updatedAt,
       state: state ?? this.state,
       city: city ?? this.city,
+      endingImageUrl: endingImageUrl ?? this.endingImageUrl,
+      endingTime: endingTime ?? this.endingTime,
+      endingImageLocation:
+          endingImageLocation ?? this.endingImageLocation, // Updated copyWith
     );
   }
 }
--- lib/models/indian_state.dart
+++ lib/models/indian_state.dart
@@ -5,28 +5,34 @@ class IndianState {
   String statename;
   List<IndianCity> cities;
 
-  IndianState({required this.stateid, required this.statename, required this.cities});
+  IndianState(
+      {required this.stateid, required this.statename, required this.cities});
 
   factory IndianState.fromJson(Map<String, dynamic> json) {
-    var list = json['cities'] as List;
-    List<IndianCity> citiesList = list.map((i) => IndianCity.fromJson(i)).toList();
+    var list = json['cities'];
+
+    List<IndianCity> citiesList =
+        List<IndianCity>.from(list.map((i) => IndianCity.fromJson(i)));
 
     return IndianState(
       stateid: json['stateid'],
       statename: json['statename'],
-      cities: citiesList,
+      cities: citiesList ?? [],
     );
   }
 
   // fromMap
   factory IndianState.fromMap(Map<String, dynamic> map) {
-    var list = map['cities'] as List;
-    List<IndianCity> citiesList = list.map((i) => IndianCity.fromMap(i)).toList();
+    // var list = map['cities'] ?? [];
+    // List<IndianCity> citiesList =
+    //     list.map((i) => IndianCity.fromMap(i)).toList();
 
     return IndianState(
       stateid: map['stateid'],
       statename: map['statename'],
-      cities: citiesList,
+      cities: map['cities'] != null
+          ? map["cities"].map((i) => IndianCity.fromMap(i)).toList()
+          : [],
     );
   }
 
